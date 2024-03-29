bfd2244112da61f023380c605538ff1b0689a610
sidsurana <125166916+sidsurana@users.noreply.github.com>
2024-03-28 22:50:15 -0400

    Update server.py
    updated connection

--- server.py
+++ server.py
@@ -1,38 +1,34 @@
 from flask import Flask, render_template, request
-#from scraper import ArticleScraper
-#from textprocessor import TextProcessor
 
 class ServerController:
     
     def __init__(self, name):
-        self.app = Flask(name)
+        self.app = Flask(name, static_folder='static', template_folder='templates')
 
-
-    # add all server endpoints using this method
-    # endpoint is the route to attach to the handler
-        # '/example'
-    # route parameters are defined inside angle brackets in the endpoint
-        # '/index/<param>'
-    # and in the handler function as an argument
-        # index(param)
-    # route methods are GET, HEAD by default, additional can be defined in the methods param as a list of strings
-        # methods=['GET', 'POST', etc]
     def add_endpoint(self, endpoint: str, endpoint_name: str, handler, methods=None):
         self.app.add_url_rule(rule=endpoint, endpoint=endpoint_name, view_func=handler, methods=methods)
 
-    # add all routes here
     def init_routes(self):
-        self.add_endpoint('/', 'index', index, methods=['GET', 'POST'])
-        self.add_endpoint('/display', 'display', display)
+        self.add_endpoint('/', 'index', self.index, methods=['GET', 'POST'])
+        self.add_endpoint('/display', 'display', self.display)
 
     def run(self, debug: bool):
         self.app.run(debug=debug)
 
-def index():
-    return render_template('index.html')
+    def index(self):
+        # Here, you can add any logic needed for initial page load or form submission handling
+        return render_template('index.html')
 
-def display():
-    if request.method == 'POST':
-        print(request.form['text'])
-    ticker = "ABCD"
-    return render_template('display.html', ticker=ticker)
+    def display(self):
+        # If you're expecting text input from a form, ensure the form's name attribute matches
+        if request.method == 'POST':
+            text_input = request.form.get('text', '')  # Assuming input has name='text'
+            print(text_input)
+        ticker = "ABCD"  # This is just a placeholder
+        # You can pass variables to the template to be rendered dynamically
+        return render_template('display.html', ticker=ticker)
+
+if __name__ == "__main__":
+    app_controller = ServerController(__name__)
+    app_controller.init_routes()
+    app_controller.run(debug=True)
